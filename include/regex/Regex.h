6163d4aedd7bed78ce026132479af6993aae8f26
Gavin Frazar <gavinfrazar@ucsb.edu>
2019-01-06 20:28:25 -0800

    Add Regex class header

---
+++ include/regex/Regex.h
@@ -0,0 +1,54 @@
+#ifndef REGEX_REGEX_H
+#define REGEX_REGEX_H
+
+#include <initializer_list>
+#include <string>
+#include "../../src/regex/RangeSet.h"
+
+namespace Regex {
+class Regex {
+ public:
+  virtual std::string toString() = 0;
+};
+
+class Chars : public Regex {
+ public:
+  Chars();
+  Chars(const CharSet &chars);
+  const CharSet chars;
+  virtual std::string toString() override;
+};
+
+class Concatenate : public Regex {
+ public:
+  Concatenate(const Regex &a, const Regex &b);
+  const Regex &a;
+  const Regex &b;
+  virtual std::string toString() override;
+};
+
+class Union : public Regex {
+ public:
+  Union(const Regex &a, const Regex &b);
+  const Regex &a;
+  const Regex &b;
+  virtual std::string toString() override;
+};
+
+class KleeneStar : public Regex {
+ public:
+  KleeneStar(const Regex &a);
+  const Regex &a;
+  virtual std::string toString() override;
+};
+
+// TODO: Make this a singleton.
+class EmptyString : public Regex {
+ public:
+  EmptyString() = default;
+  virtual std::string toString() override;
+};
+
+}  // namespace Regex
+
+#endif
