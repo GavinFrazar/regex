100d5f056959b2dfd14618f64b9559c86f710fa2
Gavin Frazar <gavinfrazar@gmail.com>
2019-01-08 23:37:08 -0800

    Change class members to smart pointers and add polymorphic equals and clone methods

--- include/regex/Regex.h
+++ include/regex/Regex.h
@@ -2,46 +2,69 @@
 #define REGEX_REGEX_H
 
 #include <initializer_list>
+#include <iostream>
+#include <memory>
 #include <string>
+#include <typeinfo>
 #include "../../src/regex/RangeSet.h"
 
 namespace Regex {
 class Regex {
  public:
   virtual std::string toString() const = 0;
+  virtual std::unique_ptr<Regex> clone() const = 0;
+  virtual bool equals(const Regex &other) const = 0;
 };
 
+using std::unique_ptr;
 class Chars : public Regex {
  public:
   Chars();
+  Chars(const Chars &other) : chars(other.chars) {}
   Chars(std::initializer_list<char> chars);
   Chars(std::initializer_list<CharSet::Interval> intervals);
   explicit Chars(const CharSet &chars);
   const CharSet chars;
   virtual std::string toString() const override;
+  virtual unique_ptr<Regex> clone() const override {
+    return unique_ptr<Chars>(new Chars(*this));
+  }
+  virtual bool equals(const Regex &other) const override;
 };
 
 class Concatenate : public Regex {
  public:
   Concatenate(const Regex &a, const Regex &b);
-  const Regex &a;
-  const Regex &b;
+  Concatenate(const Concatenate &other);
+  Concatenate(unique_ptr<Regex> a, unique_ptr<Regex> b);
+  unique_ptr<Regex> a, b;
   virtual std::string toString() const override;
+  virtual unique_ptr<Regex> clone() const override {
+    return unique_ptr<Concatenate>(new Concatenate(*this));
+  }
+  virtual bool equals(const Regex &other) const override;
 };
 
 class Union : public Regex {
  public:
   Union(const Regex &a, const Regex &b);
-  const Regex &a;
-  const Regex &b;
+  Union(const Union &other);
+  unique_ptr<Regex> a, b;
   virtual std::string toString() const override;
+  unique_ptr<Regex> clone() const override;
+  // STUB
+  virtual bool equals(const Regex &other) const override { return true; }
 };
 
 class KleeneStar : public Regex {
  public:
   KleeneStar(const Regex &a);
-  const Regex &a;
+  KleeneStar(const KleeneStar &other);
+  unique_ptr<Regex> a;
   virtual std::string toString() const override;
+  unique_ptr<Regex> clone() const override;
+  // STUB
+  virtual bool equals(const Regex &other) const override { return true; }
 };
 
 // TODO: Make this a singleton.
@@ -49,12 +72,20 @@ class EmptyString : public Regex {
  public:
   EmptyString() = default;
   virtual std::string toString() const override;
+  virtual unique_ptr<Regex> clone() const override {
+    // STUB
+    return unique_ptr<Regex>();
+  }
+  // STUB
+  virtual bool equals(const Regex &other) const override { return true; }
 };
 
- bool operator==(const Chars &a, const Chars &b);
- bool operator==(const Concatenate &a, const Concatenate &b);
- bool operator==(const Union &a, const Union &b);
- bool operator==(const KleeneStar &a, const KleeneStar &b);
+bool operator==(const Regex &a, const Regex &b);
+bool operator==(const unique_ptr<Regex> &a, const unique_ptr<Regex> &b);
+bool operator==(const Chars &a, const Chars &b);
+bool operator==(const Concatenate &a, const Concatenate &b);
+bool operator==(const Union &a, const Union &b);
+bool operator==(const KleeneStar &a, const KleeneStar &b);
 
 }  // namespace Regex
 
--- src/regex/Regex.cpp
+++ src/regex/Regex.cpp
@@ -6,10 +6,9 @@ Chars::Chars() : chars() {}
 Chars::Chars(std::initializer_list<char> chars) : chars(CharSet(chars)) {}
 
 Chars::Chars(std::initializer_list<CharSet::Interval> intervals)
-  : chars(CharSet(intervals))
-{}
+    : chars(CharSet(intervals)) {}
 
-Chars::Chars(const CharSet& chars) : chars(chars) {}
+Chars::Chars(const CharSet &chars) : chars(chars) {}
 
 std::string Chars::toString() const {
   if (chars.isEmpty()) {
@@ -19,32 +18,72 @@ std::string Chars::toString() const {
   }
 }
 
-Concatenate::Concatenate(const Regex& a, const Regex& b) : a(a), b(b) {}
-
-std::string Concatenate::toString() const {
-  return "(" + a.toString() + " ~ " + b.toString() + ")";
+bool Chars::equals(const Regex &other) const {
+  const Chars &other_derived = dynamic_cast<const Chars &>(other);
+  return *this == other_derived;
 }
 
-Union::Union(const Regex& a, const Regex& b) : a(a), b(b) {}
+Concatenate::Concatenate(const Regex &a, const Regex &b)
+    : a(a.clone()), b(b.clone()) {}
 
-  std::string Union::toString() const { return "(" + a.toString() + " | "
-      + b.toString() + ")";}
+Concatenate::Concatenate(const Concatenate &other)
+    : a(other.a->clone()), b(other.b->clone()) {}
 
-KleeneStar::KleeneStar(const Regex& a) : a(a) {}
-  std::string KleeneStar::toString() const { return "(" + a.toString() +")*"; }
-  std::string EmptyString::toString() const { return "ε"; }
+std::string Concatenate::toString() const {
+  return "(" + a->toString() + " ~ " + b->toString() + ")";
+}
 
-  bool operator==(const Chars &a, const Chars &b){
-    return a.chars == b.chars;
+bool Concatenate::equals(const Regex &other) const {
+  const Concatenate &other_derived = dynamic_cast<const Concatenate &>(other);
+  return *this == other_derived;
+}
+
+Union::Union(const Regex &a, const Regex &b) : a(a.clone()), b(b.clone()) {}
+
+Union::Union(const Union &other) : a(other.a->clone()), b(other.b->clone()) {}
+
+std::string Union::toString() const {
+  return "(" + a->toString() + " | " + b->toString() + ")";
+}
+
+unique_ptr<Regex> Union::clone() const {
+  return unique_ptr<Union>(new Union(*this));
+}
+
+KleeneStar::KleeneStar(const Regex &a) : a(a.clone()) {}
+
+KleeneStar::KleeneStar(const KleeneStar &other) : a(other.a->clone()) {}
+
+unique_ptr<Regex> KleeneStar::clone() const {
+  return unique_ptr<KleeneStar>(new KleeneStar(*this));
+}
+
+std::string KleeneStar::toString() const { return "(" + a->toString() + ")*"; }
+
+std::string EmptyString::toString() const { return "ε"; }
+
+bool operator==(const Regex &a, const Regex &b) {
+  if (typeid(a) == typeid(b)) {
+    return a.equals(b);
   }
-  bool operator==(const Concatenate &a, const Concatenate &b){
-    return a.a == b.a && a.b == b.b;
-  }
-  bool operator==(const Union &a, const Union &b){
-    return a.a == b.a && a.b == b.b;
-  }
-  bool operator==(const KleeneStar &a, const KleeneStar &b){
-    return a.a == b.a;
-  }
+  return false;
+}
+
+bool operator==(const unique_ptr<Regex> &a, const unique_ptr<Regex> &b) {
+  return *a == *b;
+}
+bool operator==(const Chars &a, const Chars &b) { return a.chars == b.chars; }
+
+bool operator==(const Concatenate &a, const Concatenate &b) {
+  return *(a.a) == *(b.a);
+}
+
+bool operator==(const Union &a, const Union &b) {
+  return *(a.a) == *(b.a) && *(a.b) == *(b.b);
+}
+
+bool operator==(const KleeneStar &a, const KleeneStar &b) {
+  return *(a.a) == *(b.a);
+}
 
 }  // namespace Regex
