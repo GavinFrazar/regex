1ed742233fa1c84a04b28c1c87eb64e16f14e0ef
Gavin Frazar <gavinfrazar@gmail.com>
2019-01-08 23:33:26 -0800

    apply clang-format to RangeSet

--- src/regex/RangeSet.cpp
+++ src/regex/RangeSet.cpp
@@ -2,6 +2,7 @@
 
 namespace Regex {
 CharSet::CharSet(const IndexedSeq<Interval>& intervals) : RangeSet(intervals) {}
-CharSet::CharSet(const std::initializer_list<Interval> intervals) : RangeSet(intervals) {}
+CharSet::CharSet(const std::initializer_list<Interval> intervals)
+    : RangeSet(intervals) {}
 CharSet::CharSet(const std::initializer_list<char> chars) : RangeSet(chars) {}
 }  // namespace Regex
